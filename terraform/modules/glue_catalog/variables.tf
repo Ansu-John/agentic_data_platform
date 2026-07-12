5b5770af14f76d02382513577bf54642ebad932e
Ansu <ansujohn09@gmail.com>
2026-07-12 18:13:14 +0530

    fix: satisfy glue_catalog module variable requirements

--- terraform/modules/glue_catalog/variables.tf
+++ terraform/modules/glue_catalog/variables.tf
@@ -2,19 +2,3 @@ variable "database_name" {
   type        = string
   description = "The name of the Glue Catalog database"
 }
-
-variable "description" {
-  type        = string
-  description = "Description of the Glue Catalog database"
-  default     = "Central metastore for the data platform"
-}
-
-variable "project" {
-  description = "The project name"
-  type        = string
-}
-
-variable "environment" {
-  description = "The environment (e.g., dev, prod)"
-  type        = string
-}
