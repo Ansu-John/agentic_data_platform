130623e77adaf44883cb856baa9672cc5dceb027
Ansu <ansujohn09@gmail.com>
2026-07-15 13:59:05 +0530

    fix: tf state path in data pipeline

--- terraform/environments/dev/03-ai-dq-agent/data.tf
+++ terraform/environments/dev/03-ai-dq-agent/data.tf
@@ -11,7 +11,7 @@ data "terraform_remote_state" "data_pipeline" {
   backend = "s3"
   config = {
     bucket = "dataplatform-dev-tfstate-bucket"
-    key    = "dev/02-data-pipeline/terraform.tfstate"
+    key    = "dev/03-ai-dq-agent/terraform.tfstate"
     region = var.aws_region
   }
 }
