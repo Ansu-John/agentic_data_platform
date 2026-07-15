954d3f6becbb1b0634f569fb6cb87c707ae25bca
Ansu <ansujohn09@gmail.com>
2026-07-15 15:24:41 +0530

    fix: s3 buckets in map

--- terraform/environments/dev/01-foundation/outputs.tf
+++ terraform/environments/dev/01-foundation/outputs.tf
@@ -6,8 +6,8 @@ output "kms_key_arn" {
 output "datalake_bucket_names" {
   description = "Map of data lake zones to their respective S3 bucket names"
   value = zipmap(
-    var.zones,
-    aws_s3_bucket.datalake_zones[*].bucket
+    var.datalake_zones,
+    module.s3_datalake.bucket_names
   )
 }
 
