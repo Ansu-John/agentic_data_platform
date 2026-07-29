7bc6ccb93f7bcf4a093b67b2fcf7fb1c05ac2efc
Ansu <ansujohn09@gmail.com>
2026-07-29 19:09:25 +0530

    update phase 2 with Aurora

--- terraform/environments/dev/02-data-pipeline/main.tf
+++ terraform/environments/dev/02-data-pipeline/main.tf
@@ -325,7 +325,7 @@ module "db_init_lambda" {
   secret_arn        = aws_rds_cluster.vector_db.master_user_secret[0].secret_arn
 
   # REQUIRED: Passing the path to the Python script so the module can zip it
-  source_code_path  = "../../../src/db"
+  source_code_path = "../../../src/db"
 
   depends_on = [aws_rds_cluster_instance.vector_db_instance]
 }
